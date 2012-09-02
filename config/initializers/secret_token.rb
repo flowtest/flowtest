728ebfed3d5f53f18ab8393dc735ce9208a73bd6
Subhash Chandra <TMaYaD@LoonyB.in>
2012-09-02 07:39:03 +0530

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowtest::Application.config.secret_token = 'a9cb9028ba52ac8b32cbf88da599cda65e29e7d3c7e820db3255f83c590a0c87e07fb25d17516f28c1682bf921edc514c65af05d5eabce2e4520dde280003672'
